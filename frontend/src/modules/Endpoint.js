93df064765edebc428f40e52aa2b2cdfe99cc64b
Andrew Wentzel <tehwentzel@gmail.com>
2024-04-10 19:06:35 +0000

    using https in endpoint, assume certs are in secret data file

--- frontend/src/modules/Endpoint.js
+++ frontend/src/modules/Endpoint.js
@@ -1,1 +1,1 @@
-export const API_URL = 'http://digitaltwin.evl.uic.edu:5000/';//this is a bad way to do this.  Whatever the flask server is set to
+export const API_URL = 'https://digitaltwin.evl.uic.edu:5000/';//this is a bad way to do this.  Whatever the flask server is set to
