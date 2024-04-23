fe5f796c2c8e58e5d729bdf0286de1fbb5fcaac2
AndrewWentzel <awentze2@uic.edu>
2024-04-23 18:00:44 -0500

    sh

--- docker/backend/init.sh
+++ docker/backend/init.sh
@@ -1,9 +1,9 @@
+export PATH=./node_modules/.bin:$PATH
 #!/bin/bash
 set -e
 echo "Running backend init"
 tmux new -d -s backend "gunicorn -w 4 -b 0.0.0.0:5000 --chdir /workspace/DigitalTwinVis/Backend App:app"
 cd /workspace/DigitalTwinVis/frontend
-export PATH=./node_modules/.bin:$PATH
 npm run build
 npm install -g serve
 serve -s build -p 8000 --cors
