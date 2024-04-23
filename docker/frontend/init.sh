3e37678e3b4c5dbf3d7e3950f91abb75fda1bc93
AndrewWentzel <awentze2@uic.edu>
2024-04-23 16:30:22 -0500

    pain

--- docker/frontend/init.sh
+++ docker/frontend/init.sh
@@ -2,5 +2,5 @@ set -e
 echo "rnning frontend init"
 cd /workspace/DigitalTwinVis/frontend
 apt-get install -y ufw
-ufw allow 80/tcp
-serve -s build -p 80 --cors
+ufw allow 8000/tcp
+serve -s build -p 8000 --cors
