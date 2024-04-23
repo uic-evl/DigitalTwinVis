a9905ce8adecea386ee35286e36c620ec553f75a
AndrewWentzel <awentze2@uic.edu>
2024-04-23 17:57:39 -0500

    sh

--- docker/backend/init.sh
+++ docker/backend/init.sh
@@ -3,7 +3,6 @@ set -e
 echo "Running backend init"
 tmux new -d -s backend "gunicorn -w 4 -b 0.0.0.0:5000 --chdir /workspace/DigitalTwinVis/Backend App:app"
 cd /workspace/DigitalTwinVis/frontend
-apt-get install -y npm
 npm run build
 npm install -g serve
 serve -s build -p 8000 --cors
