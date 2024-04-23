69d5ce21c0c7a3a29358a2a9bbc0da9d6dc3c13f
AndrewWentzel <awentze2@uic.edu>
2024-04-23 17:32:42 -0500

    a single semicolon

--- docker/backend/init.sh
+++ docker/backend/init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 echo "Running backend init"
-gunicorn -w 4 -b 0.0.0.0:5000 --chdir /workspace/DigitalTwinVis/Backend App:app
+tmux new -d -s backend "gunicorn -w 4 -b 0.0.0.0:5000 --chdir /workspace/DigitalTwinVis/Backend App:app"
+cd /workspace/DigitalTwinVis/frontend
+npm run build
+npm install -g serve
+serve -s build -p 8000 --cors
