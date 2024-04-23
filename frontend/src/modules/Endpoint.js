4f6a397fa6036423d2eef6fe2576aa710215556f
AndrewWentzel <awentze2@uic.edu>
2024-04-23 16:58:57 -0500

    a single semicolon

--- frontend/src/modules/Endpoint.js
+++ frontend/src/modules/Endpoint.js
@@ -1,2 +1,2 @@
 
-export const API_URL = 'http://digitaltwinbackend:5000/';//this is a bad way to do this.  Whatever the flask server is set to
+export const API_URL = 'https://digitaltwinproxy/api';//this is a bad way to do this.  Whatever the flask server is set to
