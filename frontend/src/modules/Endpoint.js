a6c82978df33165b50cf155ac105489c9070da9f
AndrewWentzel <awentze2@uic.edu>
2024-04-23 20:20:16 -0500

    attempt

--- frontend/src/modules/Endpoint.js
+++ frontend/src/modules/Endpoint.js
@@ -1,2 +1,2 @@
 
-export const API_URL = 'http://digitaltwin.evl.uic.edu/api';//this is a bad way to do this.  Whatever the flask server is set to
+export const API_URL = 'http://digitaltwin.evl.uic.edu/api/';//this is a bad way to do this.  Whatever the flask server is set to
