c1aea6e66280e2b7c55e6769a45a02a3326402e2
AndrewWentzel <awentze2@uic.edu>
2024-04-26 00:42:05 -0500

    undid changes to add dass because servers are no good

--- docker/dass_backend/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "Running backend init"
-gunicorn -w 3 -b 0.0.0.0:8080 --chdir /workspace/DigitalTwinVis/DASS/dass_backend App:app
-
--- docker/dass_frontend/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /workspace/DigitalTwinVis/DASS/dass_frontend
-npm run build
-npm install -g serve
-serve -s build -p 9000 --cors
