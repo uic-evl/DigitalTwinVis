aae3761b055b0533d35570140393e38079ffcd17
ScheWann <92758590+ScheWann@users.noreply.github.com>
2024-05-06 22:04:04 -0400

    change the smoke status to the discrete type

--- frontend/src/modules/Constants.js
+++ frontend/src/modules/Constants.js
@@ -22,6 +22,7 @@ export const ordinalVars = {
         'N-category': [0,1,2,3],
         'T-category': [1,2,3,4],
         'Pathological Grade': [0,1,2,3,4],
+        'smoking_status': [0.0,0.5,1.0],
         // 'hpv': [-1,0,1]
     }
 export const booleanVars = [
@@ -35,7 +36,6 @@ export const continuousVars = [
 'age',
 'hpv',
 'total_dose',
-'smoking_status',
 'dose_fraction',
 'packs_per_year'
 ]
